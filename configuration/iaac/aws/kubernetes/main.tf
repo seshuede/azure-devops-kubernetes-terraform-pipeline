9e11000f89c868cd3b8f2c98a80af8d39839f159
seshuede@gmail.com <seshuede@gmail.com>
2020-02-29 16:14:11 -0500

    update

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,13 +1,13 @@
 # aws --version
-# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
+# aws eks --region us-east-1 update-kubeconfig --name nede1-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
+# terraform-backend-state-nede1-123
 # AKIA4AHVNOD7OOO6T4KI
 
 
 terraform {
   backend "s3" {
-    bucket = "mybucket" # Will be overridden from build
+    bucket = aws_s3_bucket.my_s3_bucket.bucket # "mybucket" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
     region = "us-east-1"
   }
@@ -29,12 +29,12 @@ provider "kubernetes" {
   version                = "~> 1.9"
 }
 
-module "in28minutes-cluster" {
+module "nede1-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
+  cluster_name    = "nede1-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
-  #subnets = data.aws_subnet_ids.subnets.ids
+  #subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
+  subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
   #vpc_id         = "vpc-1234556abcdef"
@@ -50,11 +50,11 @@ module "in28minutes-cluster" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.nede1-cluster.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.nede1-cluster.cluster_id
 }
 
 
